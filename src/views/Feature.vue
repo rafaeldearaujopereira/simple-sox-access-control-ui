46969cf4135858e41cd6b2b918df77fabd7e786d
rafaeldearaujopereira <rafael.a.pereira@gmail.com>
2021-03-01 23:06:15 -0300

    design for forms

--- src/views/Feature.vue
+++ src/views/Feature.vue
@@ -1,13 +1,28 @@
 <template>
-    <div class="text-start">
-        <h2 class="text-start p-3">Features</h2>
-        <form>
-            <div class="mb-3 p-3">
-            <label for="description" class="form-label">Description</label>
-            <input type="text" class="form-control" id="description" />
+    <h2 class="text-start ms-3 p-3">Features</h2>
+    <div class="container">
+        <form class="row g-3">
+            <div class="col-md-3">
+                <label for="name" class="form-label">Name</label>
+                <input type="text" class="form-control" id="name" />
+            </div>
+            <div class="col-md-4">
+                <label for="description" class="form-label">Description</label>
+                <input type="text" class="form-control" id="description" />
+            </div>
+            <div class="col-md-1">
+                <label for="code" class="form-label">Code</label>
+                <input type="text" class="form-control" id="code" />
+            </div>
+            <div class="col-md-7">
+                <label for="parent" class="form-label">Parent</label>
+                <select class="form-select" id="parent">
+                    <option selected disabled value></option>
+                    <option value="2">Config > Users</option>
+                </select>
             </div>
             
-            <button type="submit" class="btn btn-primary ms-3">Search</button>
+            <button type="submit" class="btn btn-primary">Search</button>
         </form>
     </div>
 </template>
